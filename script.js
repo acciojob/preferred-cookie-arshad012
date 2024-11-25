436ff612d228e1d0c0052a9aabf3db08e83bdee1
arshad012 <forarshad99@gmail.com>
2024-11-25 15:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.querySelector('form').addEventListener('submit', () => {
+	event.preventDefault();
+
+	const size = document.getElementById('fontsize').value;
+	const color = document.getElementById('fontcolor').value;
+
+	localStorage.setItem('cookies', JSON.stringify({size,color}));
+})
+
+window.onload = () => {
+	// const root = document.querySelector(':root');
+	const styles = JSON.parse(localStorage.getItem('cookies'));
+	const body = document.querySelector('body');
+	body.style.color = styles.color;
+	body.style.fontSize = styles.size + 'px';
+}
+
+
